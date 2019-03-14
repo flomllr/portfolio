db6d0d575a583bbe16fabc00bf59cb015c950d3c
Florian Mueller <contact@flomllr.com>
2019-03-14 01:53:26 +0100

    Add tilt

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Tilt from 'react-vanilla-tilt';
+import Tilt from 'react-tilt';
 import '../style/Project.css';
 
 function Gallery(props) {
   const { title, subtitle, description, url, linktext, techstack } = props;
   return (
-    <Tilt className="Project">
+    <Tilt className="Project" options={{ reverse: true, scale: 1, max: 15 }}>
       <h3>{subtitle}</h3>
       <h4>{title}</h4>
       <p>{description}</p>
