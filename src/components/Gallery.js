6d9453e1c7dd60910746924233f36f00338177e4
Florian Mueller <contact@flomllr.com>
2019-09-17 22:21:38 +0200

    Clean start

--- src/components/Gallery.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../style/Gallery.css';
-
-function Gallery(props) {
-  const { children } = props;
-  return <div className="Gallery">{children}</div>;
-}
-export default Gallery;
